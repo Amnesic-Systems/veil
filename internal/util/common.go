9a795e55749feb5d8665f7cb9abf5bf7f95093ab
Philipp Winter <phw@nymity.ch>
2024-10-20 08:06:49 -0500

    Remove unused function.

--- internal/util/common.go
+++ internal/util/common.go
@@ -4,12 +4,6 @@ func AddrOf[T any](v T) *T {
 	return &v
 }
 
-func PanicOnErr[T any](err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 func Must[T any](v T, err error) T {
 	if err != nil {
 		panic(err)
