73269bd39568c7b4f15a05b4767eb0ad288bf472
Philipp Winter <phw@amnesic.systems>
2024-12-30 16:02:23 -0600

    Add comments.
    ...for both packages and functions.

--- internal/errs/errs.go
+++ internal/errs/errs.go
@@ -1,3 +1,4 @@
+// Package errs provides convenience functions for error handling.
 package errs
 
 import (
@@ -11,18 +12,23 @@ var (
 	IsNil         = errors.New("argument must not be nil")
 )
 
+// Wrap wraps the given error using the given string and (if provided) string
+// arguments. The function replaces the given error with the new error.
 func Wrap(err *error, str string, args ...any) {
 	if *err != nil {
 		*err = fmt.Errorf("%s: %w", fmt.Sprintf(str, args...), *err)
 	}
 }
 
+// WrapErr works like `Wrap` but takes as input a second error instead of a
+// string.
 func WrapErr(err *error, new error) {
 	if *err != nil {
 		*err = fmt.Errorf("%w: %w", new, *err)
 	}
 }
 
+// Add works like `Wrap` but returns the new error instead.
 func Add(err error, str string, args ...any) error {
 	if err == nil {
 		return nil
@@ -30,6 +36,9 @@ func Add(err error, str string, args ...any) error {
 	return fmt.Errorf("%s: %w", fmt.Sprintf(str, args...), err)
 }
 
+// Join joins the two given errors using `errors.Join`. The only difference to
+// `errors.Join` is that this function takes as input an error pointer, which
+// allows us to omit re-assignment.
 func Join(origErr *error, new error) {
 	if origErr == nil {
 		return
